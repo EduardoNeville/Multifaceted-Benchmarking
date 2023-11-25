f87d3ce7ebc19ded7fd1f423d030d64ee28adec1
EduardoNeville <57065449+EduardoNeville@users.noreply.github.com>
2023-11-25 23:58:43 +0100

    added requirements and started mistral test runs

--- src/test-runs/mistral-drag.py
+++ src/test-runs/mistral-drag.py
@@ -1,5 +1,17 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM  
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("dragon-mistral-7b-v0")  
-model = AutoModelForCausalLM.from_pretrained("dragon-mistral-7b-v0")  
+tokenizer = AutoTokenizer.from_pretrained("models/OpenHermes-2.5-Mistral-7B/tokenizer.model")
+model = AutoModelForCausalLM.from_pretrained("models/OpenHermes-2.5-Mistral-7B/")
+
+print(tokenizer)
+print(model)
+
+prompt= "Hello how are you"
+
+model_inputs = tokenizer([prompt], return_tensor="pt")
+
+generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample= True)
+print(tokenizer.batch_decode(generated_ids)[0])
+
 
