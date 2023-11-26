99661341bfb9d567f853ba3fdde612e8fa88675e
EduardoNeville <57065449+EduardoNeville@users.noreply.github.com>
2023-11-26 01:36:12 +0100

    modifs to mistral-drag

--- src/test-runs/mistral-drag.py
+++ src/test-runs/mistral-drag.py
@@ -1,17 +1,20 @@
 # Load model directly
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("models/OpenHermes-2.5-Mistral-7B/tokenizer.model")
-model = AutoModelForCausalLM.from_pretrained("models/OpenHermes-2.5-Mistral-7B/")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+tokenizer = AutoTokenizer.from_pretrained("models/OpenHermes-2.5-Mistral-7B")
+model = AutoModelForCausalLM.from_pretrained("models/OpenHermes-2.5-Mistral-7B")
 
 print(tokenizer)
 print(model)
 
 prompt= "Hello how are you"
 
-model_inputs = tokenizer([prompt], return_tensor="pt")
+model_inputs = tokenizer(prompt, return_tensor="pt")
 
-generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample= True)
+generated_ids = model.generate(**model_inputs, max_new_tokens=100)
 print(tokenizer.batch_decode(generated_ids)[0])
 
 
