066dc78259191ef449bfaf8d381736ad6724d6b1
EduardoNeville <57065449+EduardoNeville@users.noreply.github.com>
2023-11-26 03:08:24 +0100

    submodule

---
+++ multiBenchmark.sh
@@ -0,0 +1,22 @@
+#!usr/bin/bash
+
+# Script to run multiple benchmarks at once
+
+benchmarks=('machiavelli' 'ethics')
+
+store_benchmarks=()
+function main($0, $1){
+    echo "Choose benchmarks: \n"
+    echo "1. Machiavelli \n"
+    echo "2. Ethics \n"
+    echo "3. \n"
+
+}
+
+function machiavelli($model_name, $runs){
+    cd benchmarks/machiavelli/
+
+    python -m generate_trajectories -a 
+}
+
+
