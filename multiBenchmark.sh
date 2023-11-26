8c9f6fa075c552147d07997e78e712fe9b444540
EduardoNeville <57065449+EduardoNeville@users.noreply.github.com>
2023-11-26 03:22:08 +0100

    latest version

--- multiBenchmark.sh
+++ multiBenchmark.sh
@@ -7,7 +7,7 @@ benchmarks=('machiavelli' 'ethics')
 store_benchmarks=()
 function main($0, $1){
     echo "Choose benchmarks: \n"
-    echo "1. Machiavelli [y/n] \n"
+    echo "1. Machiavelli [y/n]: \n"
     echo "2. Ethics [y/n] \n"
     echo "3. \n"
 
@@ -18,6 +18,8 @@ function machiavelli($model_name, $runs){
     cd benchmarks/machiavelli/
 
     python -m generate_trajectories -a 
+
+
 }
 
 
